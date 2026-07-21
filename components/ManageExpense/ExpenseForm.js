8afb929cc0a501d7ac34371fc11dfcfa8bfdec43
Sam Ridsdale <samridsdale108@gmail.com>
2026-07-21 20:29:05 +0100

    Building a custom input component

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,36 @@
+import { View } from "react-native";
+import Input from "./Input";
+
+function ExpenseForm() {
+  function amountChangedHandler() {}
+
+  return (
+    <View>
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangedHandler,
+        }}
+      />
+      <Input
+        label="date"
+        textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input
+        label="Description"
+        textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+    </View>
+  );
+}
+
+export default ExpenseForm;
