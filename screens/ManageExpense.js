9d1242865e5d9b8dbfc9a8833afc5abec7ef5042
Sam Ridsdale <samridsdale108@gmail.com>
2026-07-21 20:02:32 +0100

    Deleting + Updating Expenses

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,10 +1,13 @@
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
+import { ExpensesContext } from "../store/expenses-context";
 
 function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
+
   const editedExpenseId = route.params?.expenseId;
   // !! converts a value into a boolean, truthy to true and vice versa
   const isEditing = !!editedExpenseId;
@@ -16,6 +19,7 @@ function ManageExpense({ route, navigation }) {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
 
@@ -24,6 +28,20 @@ function ManageExpense({ route, navigation }) {
   }
 
   function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, {
+        description: "A Fat Boat",
+        amount: 29.99,
+        date: new Date("2026-07-21"),
+      });
+    } else {
+      // requires user input
+      expensesCtx.addExpense({
+        description: "A Boat",
+        amount: 19.99,
+        date: new Date("2026-07-21"),
+      });
+    }
     navigation.goBack();
   }
 
